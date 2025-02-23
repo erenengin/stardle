580738348e9704a13f232e956af7bb64ebe7ea76
eren <erenengin3439@gmail.com>
2025-02-23 20:26:07 +0300

    Fix

--- src/App.js
+++ src/App.js
@@ -113,7 +113,8 @@ function App() {
     else feedback.push('gray');
 
     if (charData.marriageable === dailyCharData.marriageable) feedback.push('green');
-    else if (charData.marriageable && dailyCharData.marriageable) feedback.push('red');
+    else if (charData.marriageable !== dailyCharData.marriageable) feedback.push('red');
+    else feedback.push('gray');
 
     if (charData.personality_trait === dailyCharData.personality_trait) feedback.push('green');
     else if (charData.personality_trait && dailyCharData.personality_trait) feedback.push('yellow');
